84433f4c06cae3f74cf74d066af08e820be0e4d6
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-01-05 00:29:26 +0530

    Compleate to login and sign in part

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ const app = express();
 require("./configs/dbConnection.js");
 const auth = require("./routes/auth_routes")
 
-
-app.use("/api/v1", auth);
-
 app.get("/", (req, res) => {
     console.log("Hello");
 })
+
+app.use(express.json());
+app.use("/api/v1", auth);
 
 app.listen(1000, () => {
     console.log("🚀💀 Server is started!");
